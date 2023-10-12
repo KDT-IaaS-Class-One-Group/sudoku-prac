b1e2eab6ea11f7be8d5a95336f4b2d88715a2684
kimhyun <forhn37@gmail.com>
2023-10-12 16:47:57 +0900

    time, score 모듈 완성

--- scoretime.js
+++ scoretime.js
@@ -1,9 +1,9 @@
 // 스코어 초기화 생성 모듈 밖에 있어야 함
-// let score = 0;
+let score = 0;
 // 삭제시 1씩 증가, 생성모듈 안에 있어야함
-// score++;
+score++;
 // 스코어값이 증가하며 최종적으로 스코어가 html파일에 표시됨
-// scorehow.texContent = score;
+scorehow.texContent = score;
 
 function limittime(num) { 
   let setting = setInterval(function() {
@@ -21,7 +21,8 @@ function limittime(num) {
 }, 1000);
 }
 
-console.log(limittime(20));
+// console.log(limittime(20));
+
 
 
 
