7bec7c3f73ae93b6b2fcbae10843c768a867065c
kimhyun <forhn37@gmail.com>
2023-10-12 16:34:22 +0900

    스코어 타임 모듈 제작시 타임조건 설정시 의문점 발생

--- scoretime.js
+++ scoretime.js
@@ -1,1 +1,15 @@
-console.log("helloworld");
+// 스코어 초기화 생성 모듈 밖에 있어야 함
+let score = 0;
+// 삭제시 1씩 증가, 생성모듈 안에 있어야함
+score++;
+// 스코어값이 증가하며 최종적으로 스코어가 html파일에 표시됨
+temp.texContent = score;
+
+setInterval(function() {
+  setTimeout(function() {
+    
+  })
+},1000)
+
+
+
