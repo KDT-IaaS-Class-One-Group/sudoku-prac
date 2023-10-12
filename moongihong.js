8ce4fd6103211be5371d9fd7503cf09123b18026
YoonJunHyeon <qmdldos@gmail.com>
2023-10-12 17:11:39 +0900

    랜덤 로직과 html 동기화 - 모듈 방식은 실패, js를 불러옴

--- moongihong.js
+++ moongihong.js
@@ -1,5 +1,7 @@
-div = document.getElementsByTagName("div")
-// console.log(div)
+
+
+li = document.getElementsByTagName("li")
+console.log(li)
 makeP = document.createElement("p")
 p = document.getElementsByTagName("p")
 button = document.getElementsByTagName("button")
@@ -18,26 +20,28 @@ makeTarget = function(random){
   // random = Math.floor(Math.random()*8-0 + 0)
   // => 0~7까지 나옴
   
-  div[random].appendChild(makeP)
-  p[0].style.width = "30px"
-  p[0].style.height = "30px"
+  li[random].appendChild(makeP)
+  p[0].style.width = "22vw"
+  p[0].style.height = "27vh"
   p[0].style.backgroundColor = "red"
   // p[0].style.backgroundImage = ""
 }
 
-for(i=0; i<div.length; i++){
-div[i].addEventListener("click",function(event){
+for(i=0; i<li.length; i++){
+li[i].addEventListener("click",function(event){
   if(event.target === p[0]){
     console.log(event)
+    // 스코어 올라갈 자리
+
     p[0].remove()
     makeTarget()
   }
 })
 }
 
-button[0].addEventListener("click",function(){
+// button[0].addEventListener("click",function(){
   makeTarget()
-})
+// })
 
-exports.module = makeTarget()
+exports.module = makeTarget;
 // 타겟 div 안에 p태그로 랜덤으로 생성
